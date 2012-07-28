8d179e82e5b4b634de52f2c5e1a9ce6072c67bc7
Jennifer Rice <dnjen@live.com>
2012-07-28 15:56:22 -0400

    vote button added and functional

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120728184319) do
+ActiveRecord::Schema.define(:version => 20120728193815) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(:version => 20120728184319) do
   end
 
   create_table "votes", :force => true do |t|
-    t.string   "topic_idinteger"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.integer  "topic_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
